706518e6f1cf7946141e58c7517ef0f86ab6d8f7
Felix Rieseberg <felix@felixrieseberg.com>
2017-08-02 16:51:30 +0900

    :art: Use icon for blog

---
+++ app/helpers/background-image.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import { isReachable } from '../utils/is-reachable';
+
+const {String, Helper} = Ember;
+
+export function backgroundImage([blog]) {
+    const url = blog.get('url') || '';
+    const icon = url.replace(/\/ghost\/$/i, '/favicon.ico');
+
+    return String.htmlSafe(`background-image: url("${icon}")`);
+}
+
+export default Helper.helper(backgroundImage);
